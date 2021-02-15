94a2aaff28754d550ad6b0b0ab92467474d0eace
Dmitry Bondarchuk <dimitrywebdeveloper@gmail.com>
2021-02-15 16:06:53 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Nav from "./components/nav";
+import Canvas from "./components/canvas";
+import { MainContextProvider } from "./context/main.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MainContextProvider>
+        <Nav />
+        <Canvas />
+      </MainContextProvider>
     </div>
   );
 }
